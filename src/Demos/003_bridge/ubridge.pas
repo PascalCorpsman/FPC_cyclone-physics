20f63464a04c98861565c71e97f7eb4ab5972d72
PascalCorpsman <corpsman@corpsman.de>
2025-06-23 14:25:34 +0200

    ADD: tiny progress on porting

--- src/Demos/003_bridge/ubridge.pas
+++ src/Demos/003_bridge/ubridge.pas
@@ -19,21 +19,43 @@ Unit ubridge;
 Interface
 
 Uses
-  Classes, SysUtils, uApp, ucore, uprecision, uparticle, urandom, utiming;
+  Classes, SysUtils, uApp, ucore, uprecision, uparticle, urandom, utiming, upcontacts;
 
 Const
-  ruleCount = 9;
-  maxFireworks = 1024;
+  ROD_COUNT = 6;
+  CABLE_COUNT = 10;
+  SUPPORT_COUNT = 12;
+  BASE_MASS = 1;
+  EXTRA_MASS = 10;
 
 Type
 
   { BridgeDemo }
 
   BridgeDemo = Class(MassAggregateApplication)
+  private
+    //cyclone::ParticleCableConstraint *supports;
+    //cyclone::ParticleCable *cables;
+    //cyclone::ParticleRod *rods;
 
+    massPos: Vector3;
+    massDisplayPos: Vector3;
 
-    Function GetTitle: String;
+    (**
+     * Updates particle masses to take into account the mass
+     * that's crossing the bridge.
+     *)
+    //void updateAdditionalMass();
 
+  public
+    Constructor Create(); virtual; reintroduce;
+    Destructor Destroy(); override;
+
+    Function GetTitle: String; override;
+
+    Procedure Display; override;
+    Procedure Update; override;
+    Procedure Key(akey: Char); override;
   End;
 
 Function getApplication(): Application;
@@ -44,14 +66,43 @@ Uses dglOpenGL;
 
 Function getApplication(): Application;
 Begin
-  result := BridgeDemo.Create(12);
+  result := BridgeDemo.Create;
 End;
 
 { BridgeDemo }
 
+Constructor BridgeDemo.Create;
+Begin
+  Inherited Create(12);
+  //cables(0),
+  //supports(0),
+  //rods(0),
+  massPos.create(0, 0, 0.5);
+End;
+
+Destructor BridgeDemo.Destroy;
+Begin
+  Inherited Destroy();
+End;
+
 Function BridgeDemo.GetTitle: String;
 Begin
   result := 'Cyclone > Bridge Demo';
+End;
+
+Procedure BridgeDemo.Display;
+Begin
+
+End;
+
+Procedure BridgeDemo.Update;
+Begin
+
+End;
+
+Procedure BridgeDemo.Key(akey: Char);
+Begin
+
 End;
 
 
